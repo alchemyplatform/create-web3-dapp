eaecaebe368c0af4548f1138f1567c1a55aae73c
eversmile12 <iwasvitto@gmail.com>
2022-08-03 02:43:46 +0200

    adds readme template

--- index.js
+++ index.js
@@ -14,88 +14,86 @@ import {selfDestroy, setRoot} from "./helpers/selfDestroy.js"
 import chalk from "chalk";
 let projectPath = "";
 
-console.log("Looking for alpha testers! Hit me up at vitto@alchemy.com")
+const program = new Commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments("[project-directory]")
+  .usage("<project-directory>")
+  .action((name) => {
+    projectPath = name;
+  })
+  .option("--ts, --typescript", "Initialize as a TypeScript project")
+  .parse(process.argv);
 
-// const program = new Commander.Command(packageJson.name)
-//   .version(packageJson.version)
-//   .arguments("[project-directory]")
-//   .usage("<project-directory>")
-//   .action((name) => {
-//     projectPath = name;
-//   })
-//   .option("--ts, --typescript", "Initialize as a TypeScript project")
-//   .parse(process.argv);
+async function run() {
+    try {
+        if (typeof projectPath === "string") {
+            projectPath = projectPath.trim();
+        }
 
-// async function run() {
-//     try {
-//         if (typeof projectPath === "string") {
-//             projectPath = projectPath.trim();
-//         }
+        if (!projectPath) {
+            projectPath = await prompts({
+                type: "text",
+                name: "projectPath",
+                message: "Please add a project path",
+                initial: "my-dapp",
+            }).then((data) => data.projectPath);
+            console.log(projectPath);
+        }
 
-//         if (!projectPath) {
-//             projectPath = await prompts({
-//                 type: "text",
-//                 name: "projectPath",
-//                 message: "Please add a project path",
-//                 initial: "my-dapp",
-//             }).then((data) => data.projectPath);
-//             console.log(projectPath);
-//         }
+        if (!projectPath) {
+            //exit non 0
+        }
 
-//         if (!projectPath) {
-//             //exit non 0
-//         }
+        let resolvedProjectPath = path.resolve(projectPath);
+        let dirExists = existsSync(resolvedProjectPath);
+        setRoot(resolvedProjectPath)
+        while (dirExists) {
+            projectPath = await prompts({
+                type: "text",
+                name: "projectPath",
+                message: "Please use a different project path",
+                initial: "my-dapp",
+            }).then((data) => data.projectPath);
+            console.log(projectPath);
+            resolvedProjectPath = path.resolve(projectPath);
+            dirExists = existsSync(resolvedProjectPath);
+            console.log(dirExists);
+        }
+        const projectName = path.basename(resolvedProjectPath);
 
-//         let resolvedProjectPath = path.resolve(projectPath);
-//         let dirExists = existsSync(resolvedProjectPath);
-//         setRoot(resolvedProjectPath)
-//         while (dirExists) {
-//             projectPath = await prompts({
-//                 type: "text",
-//                 name: "projectPath",
-//                 message: "Please use a different project path",
-//                 initial: "my-dapp",
-//             }).then((data) => data.projectPath);
-//             console.log(projectPath);
-//             resolvedProjectPath = path.resolve(projectPath);
-//             dirExists = existsSync(resolvedProjectPath);
-//             console.log(dirExists);
-//         }
-//         const projectName = path.basename(resolvedProjectPath);
+        const isEthereumProject = await prompts({
+            type: "select",
+            name: "virtualMachine",
+            message: "For which VM are you building for?",
+            choices: [
+                { title: "Ethereum", value: "ethereum" },
+                { title: "Solana", value: "solana" },
+            ],
+            initial: 0,
+        }).then((data) => data.virtualMachine == "ethereum" ? true : false);
 
-//         const isEthereumProject = await prompts({
-//             type: "select",
-//             name: "virtualMachine",
-//             message: "For which VM are you building for?",
-//             choices: [
-//                 { title: "Ethereum", value: "ethereum" },
-//                 { title: "Solana", value: "solana" },
-//             ],
-//             initial: 0,
-//         }).then((data) => data.virtualMachine == "ethereum" ? true : false);
+        const wantsTemplateFiles = await prompts({
+            type: "toggle",
+            name: "templateFiles",
+            message: "Do you want to import the tutorial?",
+            initial: true,
+            active: "yes",
+            inactive: "no",
+        }).then((data) => (data.templateFiles == "yes" ? true : false));
 
-//         const wantsTemplateFiles = await prompts({
-//             type: "toggle",
-//             name: "templateFiles",
-//             message: "Do you want to import the tutorial?",
-//             initial: true,
-//             active: "yes",
-//             inactive: "no",
-//         }).then((data) => (data.templateFiles == "yes" ? true : false));
+        // console.log("wantsTemplate", wantsTemplateFiles);
 
-//         // console.log("wantsTemplate", wantsTemplateFiles);
+        mkdir(resolvedProjectPath);
+        cloneRepo(resolvedProjectPath, isEthereumProject, wantsTemplateFiles);
+        console.log(chalk.green("files copied ✅"))
+        createPackageJson(
+            isEthereumProject,
+            projectName
+        );
+        cleanUpFiles();
+    } catch (e) {
+        selfDestroy(e)
+    }
+}
 
-//         mkdir(resolvedProjectPath);
-//         cloneRepo(resolvedProjectPath, isEthereumProject, wantsTemplateFiles);
-//         console.log(chalk.green("files copied ✅"))
-//         createPackageJson(
-//             isEthereumProject,
-//             projectName
-//         );
-//         cleanUpFiles();
-//     } catch (e) {
-//         selfDestroy(e)
-//     }
-// }
-
-// run();
+run();
