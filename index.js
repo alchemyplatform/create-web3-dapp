7317dd521912ee7d24c1a4b19720bc9325011560
eversmile12 <iwasvitto@gmail.com>
2022-08-04 00:39:31 +0200

    prints project name

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
 
 import * as Commander from "commander";
 import prompts from "prompts";
 import path from "path";
+import packageJson from "./package.json" assert {type:"json"}
 import { createPackageJson } from "./helpers/createPackage.js";
-import { existsSync, readFileSync } from "fs";
+import { existsSync } from "fs";
 import { mkdir } from "./helpers/mkdir.js";
 import { cleanUpFiles } from "./helpers/cleanUpFiles.js";
 import { cloneRepo } from "./helpers/cloneRepo.js";
 import {selfDestroy, setRoot} from "./helpers/selfDestroy.js"
 import chalk from "chalk";
-
-
-const packageJson = JSON.parse(readFileSync("./package.json"));
-console.log(packageJson.name)
 
 console.log(`MMMMMMMMMMMMMMMMMK:..:KMMMMMMMMMMMMMMMMM
 MMMMMMMMMMMMMMMWO,    ,OWMMMMMMMMMMMMMMM
@@ -60,6 +58,7 @@ async function run() {
                 message: "Please add a project path",
                 initial: "my-dapp",
             }).then((data) => data.projectPath);
+            console.log(projectPath);
         }
 
         if (!projectPath) {
