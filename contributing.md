173efebda2b31f519bf4d216bab9cf8ed2bf2934
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-08-08 23:03:51 +0530

    add contributing.md and lint code

---
+++ contributing.md
@@ -0,0 +1,36 @@
+# Contributing
+
+Contributions are welcome!
+
+1. Check the [ISSUES](https://github.com/Eversmile12/create-web3-dapp/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc). Read every issue to understand what's needed and whether it's something you can help with.
+
+2. Ask other contributors to see if no one has taken the issue yet. If you're interested in tackling such a feature and it's still available, we will assign you to the task.
+
+3. Clone the repo and create your own branch using `git checkout -b your_branch_name`. Remember to use a branch name that describes WHAT you're doing/fixing.
+
+4. Setup your local development environment. Instructions
+
+5. Once your work is done with the local copy of the repo, don't hesitate to open a pull request. We'll gladly revise and push as deemed fit.
+
+6. Feel free to add new issues as you read the code and find inconsistencies and/or possible features that may add up to the website. Follow the labeling standards to make it easier to understand what you're proposing.
+
+7. Document changes and/or issues clearly. Make it easy for everyone involved to understand your ideas/changes.
+
+## Local development
+
+To setup your local dev environment:
+
+```sh
+# Clone the repo
+git clone https://github.com/Eversmile12/create-web3-dapp.git
+
+cd create-web3-dapp
+
+# Install dependencies
+yarn
+
+```
+
+## Creating a pull request
+
+In order to create a pull request for create-web3-dapp, follow the GitHub instructions for [Creating a pull request from a fork](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request-from-a-fork). Please link your pull request to an existing issue.
--- helpers/utils/cloneRepo.ts
+++ helpers/utils/cloneRepo.ts
@@ -8,56 +8,56 @@ import { setUpHardhat } from "../backend_helpers/setupHardhat.js";
 import { dappInfo } from "../../interfaces/dappInfo.js";
 
 export const cloneRepo = (projectPath: string, dappInfo: dappInfo) => {
-  try {
-    process.chdir(projectPath);
-    console.log(chalk.yellow("Downloading files..."));
-    const bar1 = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
-    bar1.start(200, 0);
-    console.log("\n");
-    execSync(
-      `git clone --depth 1 ${"https://github.com/Eversmile12/create-web3-dapp"} .`
-    );
-    console.log("\n");
-    bar1.update(100);
+	try {
+		process.chdir(projectPath);
+		console.log(chalk.yellow("Downloading files..."));
+		const bar1 = new cliProgress.SingleBar(
+			{},
+			cliProgress.Presets.shades_classic
+		);
+		bar1.start(200, 0);
+		console.log("\n");
+		execSync(
+			`git clone --depth 1 ${"https://github.com/Eversmile12/create-web3-dapp"} .`
+		);
+		console.log("\n");
+		bar1.update(100);
 
-    console.log(chalk.yellow("\nCopying project files..."));
+		console.log(chalk.yellow("\nCopying project files..."));
 
-    const template = path.join(
-      process.cwd(),
-      "templates",
-      dappInfo.chain == "ethereum" ||
-        dappInfo.chain == "polygon" ||
-        dappInfo.chain == "arbitrum" ||
-        dappInfo.chain == "optimism"
-        ? "ethereum"
-        : "solana",
-      dappInfo.wantsTemplateFiles ? "tutorial" : "vanilla"
-    );
+		const template = path.join(
+			process.cwd(),
+			"templates",
+			dappInfo.chain == "ethereum" ||
+				dappInfo.chain == "polygon" ||
+				dappInfo.chain == "arbitrum" ||
+				dappInfo.chain == "optimism"
+				? "ethereum"
+				: "solana",
+			dappInfo.wantsTemplateFiles ? "tutorial" : "vanilla"
+		);
 
-    fse.copySync(template, process.cwd());
+		fse.copySync(template, process.cwd());
 
-    bar1.update(200);
-    console.log(chalk.green("Project files copied ✅"));
+		bar1.update(200);
+		console.log(chalk.green("Project files copied ✅"));
 
-    bar1.stop();
+		bar1.stop();
 
-    if (dappInfo.useBackend) {
-      console.log(chalk.yellow(`Copying ${dappInfo.backendProvider} files...`));
-      switch (dappInfo.backendProvider) {
-        case "hardhat":
-          setUpHardhat(dappInfo);
-          break;
+		if (dappInfo.useBackend) {
+			console.log(chalk.yellow(`Copying ${dappInfo.backendProvider} files...`));
+			switch (dappInfo.backendProvider) {
+				case "hardhat":
+					setUpHardhat(dappInfo);
+					break;
 
-        case "foundry":
-          break;
-        case "Anchor":
-          break;
-      }
-    }
-  } catch (e) {
-    selfDestroy();
-  }
+				case "foundry":
+					break;
+				case "Anchor":
+					break;
+			}
+		}
+	} catch (e) {
+		selfDestroy();
+	}
 };
