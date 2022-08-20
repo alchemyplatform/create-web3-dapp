f9b6708a349f6a5b1868c4eb1e7c8895e7c36e3b
kenxdrgn <ken0ryu@gmail.com>
2022-08-20 17:58:44 +0900

    fix: append filename to the destination

--- helpers/utils/copyFile.ts
+++ helpers/utils/copyFile.ts
@@ -1,13 +1,13 @@
-import path from "path"
-import fse from "fs-extra"
+import path from "path";
+import fse from "fs-extra";
 
 export const copyFile = (folder, filename, destination) => {
+	const file = path.join(process.cwd(), "templates", folder, filename);
 
-    const file = path.join(process.cwd(), "templates", folder, filename)
+	if (destination !== process.cwd()) {
+		destination = path.join(process.cwd(), destination);
+	}
+	destination = path.join(destination, filename);
 
-    if (destination !== process.cwd()) {
-        destination = path.join(process.cwd(), destination)
-    }
-    
-    fse.copySync(file, destination)
-}
+	fse.copySync(file, destination);
+};
