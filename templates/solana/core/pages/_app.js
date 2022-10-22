1622d4b2532647c95fd22ab45fc2fc5a3f438ec3
steveluscher <me+github@steveluscher.com>
2022-10-21 23:01:50 -0700

    [solana] Memoize the set of wallets to prevent losing connection on re-render

--- templates/solana/core/pages/_app.js
+++ templates/solana/core/pages/_app.js
@@ -5,14 +5,15 @@ import {
 } from "@solana/wallet-adapter-react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import * as web3 from "@solana/web3.js";
+import { useMemo } from "react";
 
 function MyApp({ Component, pageProps }) {
 	const endpoint = web3.clusterApiUrl("devnet");
-	const wallet = new PhantomWalletAdapter();
+	const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
 	return (
 		<ConnectionProvider endpoint={endpoint}>
-			<WalletProvider wallets={[wallet]}>
+			<WalletProvider wallets={wallets}>
 				<Component {...pageProps} />
 			</WalletProvider>
 		</ConnectionProvider>
