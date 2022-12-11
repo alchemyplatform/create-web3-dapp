ad96a61826b98864116a95ceda88590ea0a3aa80
ALBERT <thatguyintech@gmail.com>
2022-12-11 17:10:55 +0800

    fix breaking imports

--- templates/evm/core/layout/section.jsx
+++ templates/evm/core/layout/section.jsx
@@ -1,4 +1,4 @@
-import styles from "../../styles/Section.module.css";
+import styles from "../styles/Section.module.css";
 
 export const Section = ({ children, title, description }) => {
 	return (
