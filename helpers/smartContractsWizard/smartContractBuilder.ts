c20cec507c3cc5dee7133e010ebd7a497e0c6ef9
Eric Lau <ericglau@outlook.com>
2023-03-17 14:16:34 -0400

    Use OpenZeppelin Wizard for contract generation

--- helpers/smartContractsWizard/smartContractBuilder.ts
+++ helpers/smartContractsWizard/smartContractBuilder.ts
@@ -2,8 +2,6 @@ import fs, { existsSync } from "fs";
 import { generateERC721Template } from "./ERC721Template.js";
 import path from "path";
 import { SmartContractInfo } from "../../interfaces/SmartContractInfo.js";
-import { getSmartContractDependencies } from "./utils/getSmartContractDependencies.js";
-import { getSmartContractSuperClasses } from "./utils/getSmartContractSuperClasses.js";
 import { isERC721 } from "./utils/isERC721.js";
 import { mkdir } from "../utils/mkdir.js";
 import { createDeployScript } from "./createDeployScript.js";
@@ -18,21 +16,11 @@ export const buildSmartContract = (smartContractInfo: SmartContractInfo, backend
 		)
 	);
 
-	const dependencies = getSmartContractDependencies(smartContractInfo);
-	const licenseIdentifier = "//SPDX-License-Identifier: MIT";
-	const pragmaDeclaration = "pragma solidity ^0.8.4;";
-
 	if (isERC721(smartContractInfo)) {
 		const smartContractTemplate = generateERC721Template(
-			smartContractInfo,
-			getSmartContractSuperClasses(smartContractInfo)
+			smartContractInfo
 		);
-		writeStream.write(`${licenseIdentifier}
-${pragmaDeclaration}
-${dependencies.join("\n")}
-
-${smartContractTemplate}
-    `);
+		writeStream.write(smartContractTemplate);
 	}
 	writeStream.end();
 
