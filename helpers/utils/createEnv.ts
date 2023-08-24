21413b54a87e9967260164db12a721535f955073
Elisha Okon <eliscodavid@gmail.com>
2023-08-24 12:39:25 +0100

    Update createEnv.ts with Project_ID argument

--- helpers/utils/createEnv.ts
+++ helpers/utils/createEnv.ts
@@ -6,14 +6,23 @@ export const createEnv = (
 	apiKeys: APIKeys,
 	projectPath = "./",
 	local: boolean,
+	projectId: string // Add the PROJECT_ID parameter here
 ) => {
 	if (Object.keys(apiKeys).length) {
 		const writeStream = fs.createWriteStream(
-			path.join(projectPath, local? ".env.local" : ".env")
+			path.join(projectPath, local ? ".env.local" : ".env")
 		);
-	
+
+		// Write the PROJECT_ID for WalletConnectCloud to the environment file
+		if (projectId) {
+			writeStream.write(`PROJECT_ID=${projectId}\n`);
+		}
+
 		for (const [key, value] of Object.entries(apiKeys)) {
-			writeStream.write(`${key.toUpperCase()}=${value}\n`);
+			// Exclude PROJECT_ID from being written again
+			if (key !== "PROJECT_ID") {
+				writeStream.write(`${key.toUpperCase()}=${value}\n`);
+			}
 		}
 		writeStream.end();
 	}
