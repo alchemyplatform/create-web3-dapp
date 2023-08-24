d93c0690432a265ab2426aeb03c3d8d2f2488a39
Elisha Okon <eliscodavid@gmail.com>
2023-08-24 12:43:59 +0100

    Update apiKeys.ts

--- interfaces/apiKeys.ts
+++ interfaces/apiKeys.ts
@@ -1,9 +1,10 @@
 export interface APIKeys {
-	ALCHEMY_API_KEY: string;
-	PRIVATE_KEY?: string | null;
-	ETHERSCAN_API_KEY?: string | null;
-	ALCHEMY_NETWORK?: string | null;
-	NEXT_PUBLIC_ALCHEMY_NETWORK?: string | null;
-	NEXT_PUBLIC_DEFAULT_CHAIN?: string | null;
-	[key: string]: string | null | undefined;
+  ALCHEMY_API_KEY: string;
+  PROJECT_ID?: string; // Add PROJECT_ID property	
+  PRIVATE_KEY?: string | null;
+  ETHERSCAN_API_KEY?: string | null;
+  ALCHEMY_NETWORK?: string | null;
+  NEXT_PUBLIC_ALCHEMY_NETWORK?: string | null;
+  NEXT_PUBLIC_DEFAULT_CHAIN?: string | null;
+  [key: string]: string | null | undefined;
 }
